bd70b12baffa76dad78db33c2683eb09d96ba5f7
danila.ramaniuk <danila.ramaniuk@auto1.com>
2019-01-15 15:27:26 +0100

    test build

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 // just for deploy test
-fetch('http://localhost:3000/ico-user-list')
+fetch('http://localhost:3000/api/ico-user-list')
   .then(res => res.json())
   .then(data => console.log('test', data));
 
