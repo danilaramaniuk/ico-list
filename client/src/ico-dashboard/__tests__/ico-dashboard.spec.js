9f6c27121ded5d62f4f7c20fba495edd94339f25
danila.ramaniuk <danila.ramaniuk@auto1.com>
2019-01-16 16:42:18 +0100

    add simple snapshot test

---
+++ client/src/ico-dashboard/__tests__/ico-dashboard.spec.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ICODashboard } from '../index';
+
+const props = {
+  contributions: [
+    {
+      address: 'rnu2xb48synW6Y1AU8ZT85OX0CWBD21N',
+      currency: 'LTC',
+      value: 1822819,
+      txid: '4911922634ee442f6704ed6cf5f860dbe70e78d1c20414d6d6f2eb4bc81e1641',
+    },
+    {
+      address: '2siyirlxk5Q96IPZD5C4On4da7hyvrxf',
+      currency: 'BTC',
+      value: 2001913,
+      txid: '482fc0d2502ae357bd898f4d9aed3c5f20d0f3966c49d7d06edb0cf6f50e1ce5',
+    },
+    {
+      address: 'LQXH0O77LVLRBUQALTCXRJ4WUNLLI6KD',
+      currency: 'ETH',
+      value: 8288838,
+      txid: '2c1bc07b4f9aedf51204115d8aaaea36b3415e3168b28c660098f82cde889b3d',
+    },
+  ],
+  setItems: jest.fn(),
+};
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<ICODashboard {...props} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
