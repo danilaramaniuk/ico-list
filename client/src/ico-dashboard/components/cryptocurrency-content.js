f39c69abd099f0f33cb16da77c9c02e9f1413bf3
danila.ramaniuk <danila.ramaniuk@auto1.com>
2019-01-17 10:54:09 +0100

    start using react-chartjs-2

--- client/src/ico-dashboard/components/cryptocurrency-content.js
+++ client/src/ico-dashboard/components/cryptocurrency-content.js
@@ -1,16 +1,12 @@
 import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Bar } from 'react-chartjs';
+import { Bar } from 'react-chartjs-2';
+import styled from 'styled-components';
 
-const options = {
-  scales: {
-    xAxes: [{
-      gridLines: {
-        offsetGridLines: true,
-      },
-    }],
-  },
-};
+const BarWrapper = styled.div`
+  width: 800px;
+  height: 400px;
+`;
 
 export default class CryptocurrencyContent extends PureComponent {
   static propTypes = {
@@ -30,13 +26,28 @@ export default class CryptocurrencyContent extends PureComponent {
       datasets: [
         {
           data: contributions.map(({ value }) => value),
+          label: 'My First dataset',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
         },
       ],
     };
 
     return (
       <Fragment>
-        <Bar data={data} options={options} width={1000} height={600} />
+        <BarWrapper>
+          <Bar
+            data={data}
+            getElementAtEvent={dataset => console.log(dataset)}
+            width={100}
+            options={{
+              maintainAspectRatio: false,
+            }}
+          />
+        </BarWrapper>
         {
           contributions.map(({ currency, value, txid }) =>
               (<div key={txid}>{currency} - {value}</div>))
