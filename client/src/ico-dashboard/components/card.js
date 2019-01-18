6729ea81d688cfee26e62dab3342089bd8063074
danila.ramaniuk <danila.ramaniuk@auto1.com>
2019-01-18 12:33:12 +0100

    add card styles

--- client/src/ico-dashboard/components/card.js
+++ client/src/ico-dashboard/components/card.js
@@ -3,10 +3,28 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const CardWrapper = styled.div`
-    padding: 16px;
-    border: 1px solid;
+    padding: 36px 26px 20px 26px;
     border-radius: 8px;
     margin: 8px 0;
+    display: flex;
+    background: white;
+    margin: 32px 20px;
+`;
+
+const VerticalValue = styled.div`
+  flex: 1;
+`;
+
+const Title = styled.div`
+  color: ${props => props.theme.palette.primary.blue400};
+  text-transform: uppercase;
+  margin-bottom: 16px;
+  font-weight: bold;
+`;
+
+const Value = styled.div`
+  margin-bottom: 16px;
+  color: ${props => props.theme.palette.primary.grey};
 `;
 
 export default class Card extends PureComponent {
@@ -33,11 +51,30 @@ export default class Card extends PureComponent {
 
     return (
       <CardWrapper>
-        { index !== null && <div>bar number: { index + 1 }</div> }
-        <div>address: { address }</div>
-        <div>currency: { currency }</div>
-        <div>value: { value }</div>
-        <div>txid: { txid }</div>
+        {
+          index !== null && (
+            <VerticalValue>
+              <Title>index</Title>
+              <Value>{ index + 1 }</Value>
+            </VerticalValue>
+          )
+        }
+        <VerticalValue>
+          <Title>currency</Title>
+          <Value>{ currency }</Value>
+        </VerticalValue>
+        <VerticalValue>
+          <Title>value</Title>
+          <Value>{ value }</Value>
+        </VerticalValue>
+        <VerticalValue>
+          <Title>address</Title>
+          <Title>txid</Title>
+        </VerticalValue>
+        <VerticalValue>
+          <Value>{ address }</Value>
+          <Value>{ txid }</Value>
+        </VerticalValue>
       </CardWrapper>
     );
   }
--- client/src/main-theme.js
+++ client/src/main-theme.js
@@ -4,6 +4,7 @@ export default {
       blue100: '#f2f7fd',
       blue200: '#0392f2',
       blue300: '#34426d',
+      blue400: '#767ea0',
       grey: '#9b9b9b',
       orange: '#f28931',
       pink: '#ff6f79',
